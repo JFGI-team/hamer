07f5ee13333dd7ccaf3f4a220f672d1ad8b3a4f8
hamer <hamer1233@naver.com>
2022-07-05 21:30:48 +0900

    프로젝트 생성

---
+++ nuxt.config.ts
@@ -0,0 +1,5 @@
+import { defineNuxtConfig } from "nuxt";
+
+export default defineNuxtConfig({
+  css: ["~/assets/styles/reset.css"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="page">Index</div>
+</template>
+
+<style scoped></style>
